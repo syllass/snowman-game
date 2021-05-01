f5f1668119681fd8a14674096210f47502acdd41
syllass <aaron_holmgreen@yahoo.com>
2021-04-30 19:29:11 -0500

    basic mvp functionality done

--- app.js
+++ app.js
@@ -10,13 +10,15 @@ const wordError = document.querySelector('.word-error');
 const letterError = document.querySelector('.letter-error');
 const letterListInput = document.querySelector('.letter-list-input');
 const letterGuess = document.querySelector('.letter-guess-input');
+const chancesLeft = document.querySelector('.chances');
+let guessCounter;
 
 newGameButton.addEventListener('click', (e) => {
 	e.preventDefault();
 
-	newGameSection.style.opacity = 0;
+	newGameSection.style.display = 'none';
 	newGameSection.style.pointerEvents = 'none';
-	enterWordSection.style.opacity = 1;
+	enterWordSection.style.display = 'block';
 	enterWordSection.style.pointerEvents = 'auto';
 });
 
@@ -28,12 +30,18 @@ playButton.addEventListener('click', (e) => {
 		return (wordError.innerText = 'Please enter a valid word');
 	}
 
-	enterWordSection.style.opacity = 0;
+	enterWordSection.style.display = 'none';
 	enterWordSection.style.pointerEvents = 'none';
-	gameSection.style.opacity = 1;
+	gameSection.style.display = 'flex';
 	gameSection.style.pointerEvents = 'auto';
 
 	letterListInput.value = '';
+
+	chancesLeft.innerText = 'Start guessing letters before I melt!';
+	guessCounter = 5;
+	while (playArea.firstChild) {
+		playArea.removeChild(playArea.firstChild);
+	}
 
 	const wordArray = inputWord.value.split('');
 	wordArray.forEach((letter) => {
@@ -52,6 +60,10 @@ playButton.addEventListener('click', (e) => {
 guessButton.addEventListener('click', (e) => {
 	e.preventDefault();
 
+	if (letterError.innerText) {
+		letterError.innerText = '';
+	}
+
 	// Error handling
 	const validLetters = /^[A-Za-z]+$/;
 	if (!letterGuess.value || !letterGuess.value.match(validLetters)) {
@@ -66,15 +78,64 @@ guessButton.addEventListener('click', (e) => {
 		letters.push(letterElements[i].innerText);
 	}
 
+	// If player guesses a correct letter
 	if (letters.includes(letterGuess.value)) {
 		for (let i = 0; i < letterElements.length; i++) {
 			if (letterElements[i].innerText == letterGuess.value) {
 				letterElements[i].style.opacity = 1;
 			}
 		}
-	} else {
+		// Check if player won
+		const checkWinArray = [];
+		for (let i = 0; i < letterElements.length; i++) {
+			checkWinArray.push(letterElements[i].style.opacity);
+		}
+		const checkWin = checkWinArray.every((value) => value == '1');
+
+		// If player won
+		if (checkWin) {
+			chancesLeft.innerText = 'YOU SAVED ME FROM MELTING!!!';
+			// Back to New Game page
+			setTimeout(() => {
+				gameSection.style.display = 'none';
+				gameSection.style.pointerEvents = 'none';
+				newGameSection.style.display = 'flex';
+				newGameSection.style.pointerEvents = 'auto';
+				inputWord.value = '';
+			}, 5000);
+		}
+		// If player guesses an incorrect letter
+	} else if (!letterListInput.value.includes(letterGuess.value)) {
 		letterListInput.value += letterGuess.value;
-		
+		// Game over
+		if (guessCounter == 0) {
+			chancesLeft.innerText = 'Game Over';
+			// Back to New Game page
+			setTimeout(() => {
+				gameSection.style.display = 'none';
+				gameSection.style.pointerEvents = 'none';
+				newGameSection.style.display = 'flex';
+				newGameSection.style.pointerEvents = 'auto';
+				inputWord.value = '';
+			}, 5000);
+		}
+
+		if (guessCounter == 5) {
+			chancesLeft.innerText = 'Oh no my feet have melted';
+		} else if (guessCounter == 4) {
+			chancesLeft.innerText = 'Uh oh there goes my body';
+		} else if (guessCounter == 3) {
+			chancesLeft.innerText = "I didn't need both arms anyways";
+		} else if (guessCounter == 2) {
+			chancesLeft.innerText = 'Or any arms at all';
+		} else if (guessCounter == 1) {
+			chancesLeft.innerText = "Last chance! Please don't let me melt!";
+		}
+		guessCounter--;
+		// If player guesses the same letter that they've already guessed
+	} else {
+		letterError.innerHTML =
+			"You've already tried that letter. <br> Guess a different letter!";
 	}
 
 	// Clear input
